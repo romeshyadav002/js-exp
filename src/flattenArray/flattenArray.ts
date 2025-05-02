a68e6a097d7cf355002c2e005016513963d490d6
romeshyadav002 <raojiromesh002@gmail.com>
2025-05-02 15:03:29 +0530

    flatten object

--- src/flattenArray/flattenArray.ts
+++ src/flattenArray/flattenArray.ts
@@ -5,3 +5,26 @@ export function flattenArray(arr: any[]) {
     return acc.concat(Array.isArray(val) ? flattenArray(val) : val);
   }, []);
 }
+
+function helper(obj, parentKey, result) {
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+      if (
+        typeof obj[key] === "object" &&
+        obj[key] !== null &&
+        !Array.isArray(obj[key])
+      ) {
+        helper(obj[key], newKey, result);
+      } else {
+        result[newKey] = obj[key];
+      }
+    }
+  }
+}
+export function flattenObject(obj: any) {
+  // Your implementation
+  const result = {};
+  helper(obj, "", result);
+  return result;
+}
